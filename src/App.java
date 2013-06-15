a64b50a23049dce9cffea7a0e9d8eec35bc95d56
Denis Polulyakh <dpoluliah@gmail.com>
2013-06-16 00:01:50 +0300

    initial

---
+++ src/App.java
@@ -0,0 +1,25 @@
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author alximik
+ * @since 6/15/13 11:59 PM
+ */
+public class App {
+
+    /* ********** Constants                ******* */
+    
+    /* ********** Variables                ******* */
+    
+    /* ********** Static methods           ******* */
+
+    public static void main(String[] args) {
+        System.out.print("Hello, git!");
+    }
+    /* ********** Constructors             ******* */
+
+    /* ********** Public instance methods  ******* */
+    
+    /* ********** Private instance methods ******* */
+    
+    /* ********** Internal classes         ******* */
+}

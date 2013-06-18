db878bad7221c94fd87a2400ed0d0ae4f1968e08
Denis Polulyakh <dpoluliah@gmail.com>
2013-06-18 23:45:11 +0300

    added utils class

---
+++ src/Utils.java
@@ -0,0 +1,25 @@
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author alximik
+ * @since 6/18/13 11:43 PM
+ */
+public class Utils {
+
+    /* ********** Constants                ******* */
+    
+    /* ********** Variables                ******* */
+    
+    /* ********** Static methods           ******* */
+
+    /* ********** Constructors             ******* */
+
+    /* ********** Public instance methods  ******* */
+    public boolean isEmpty(String s) {
+        return s != null && s.length()>0;
+    }
+    
+    /* ********** Private instance methods ******* */
+    
+    /* ********** Internal classes         ******* */
+}

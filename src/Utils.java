fb1f23339cf8aa7135f1c966a1a7b9905ea828d5
Denis Polulyakh <dpoluliah@gmail.com>
2013-06-19 14:29:02 +0300

    added utils

---
+++ src/Utils.java
@@ -0,0 +1,22 @@
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author alximik
+ * @since 6/19/13 2:27 PM
+ */
+public class Utils {
+
+    /* ********** Constants                ******* */
+    
+    /* ********** Variables                ******* */
+    
+    /* ********** Static methods           ******* */
+
+    /* ********** Constructors             ******* */
+
+    /* ********** Public instance methods  ******* */
+    
+    /* ********** Private instance methods ******* */
+    
+    /* ********** Internal classes         ******* */
+}
